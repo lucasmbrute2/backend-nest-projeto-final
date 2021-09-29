0ccf2fb4be53fec64fa6ab721d5be9ed76cd4952
Lucas <lucasmbrute614@gmail.com>
2021-09-29 03:21:17 -0300

    post with relational genres

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { profile } from 'console';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
