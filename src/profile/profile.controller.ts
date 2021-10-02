7de030d2e485b21a7175d61d783fb5bc0f4b7e62
Lucas <lucasmbrute614@gmail.com>
2021-10-01 22:03:02 -0300

    setting error not found arrow

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus, HttpException } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
@@ -6,6 +6,8 @@ import { UpdateProfileDto } from './dto/update-profile.dto';
 @Controller('profile')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) { }
+
+  private readonly _errorReturn = (id: string) => { throw new HttpException('Erro', 400) }
 
   @Post()
   create(@Body() createProfileDto: CreateProfileDto) {
@@ -19,13 +21,15 @@ export class ProfileController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.profileService.findOne(+id);
+    return this.profileService.findOne(+id).catch(err => this._errorReturn(id)
+    )
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProfileDto: UpdateProfileDto) {
     return this.profileService.update(+id, updateProfileDto);
   }
+
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
