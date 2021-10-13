34f0eaed732e7e30b0cc2e8778b9ac5d2866f6e9
Lucas <lucasmbrute614@gmail.com>
2021-10-13 17:48:31 -0300

    feat(update-profile-dto) => removing partial type

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,8 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsInt } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsArray, IsInt, IsOptional, ValidateNested } from 'class-validator';
+import { UpdateProfileDto } from 'src/profile/dto/update-profile.dto';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
     @IsInt()
     id: number
+
+    @ValidateNested({ each: true })
+    @IsOptional()
+    @Type(() => UpdateProfileDto)
+    @IsArray()
+    profile?: UpdateProfileDto[];
 }
