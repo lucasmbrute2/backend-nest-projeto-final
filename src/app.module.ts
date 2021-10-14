85fbecd0145d13ac717019773d9538c5dd973c96
Lucas <lucasmbrute614@gmail.com>
2021-10-14 17:11:18 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfileModule } from './profile/profile.module';
 import { GenreModule } from './genre/genre.module';
 import { UserModule } from './user/user.module';
 import { GameModule } from './game/game.module';
+import { AppController } from './app.controller';
 
 
 @Module({
