fb0ba90031b46c5543761964b32c560d95ab1f6b
Lucas <lucasmbrute614@gmail.com>
2021-10-18 18:13:45 -0300

    chore(app.module): implement jwtAuthGuard as provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,17 @@ import { UserModule } from './user/user.module';
 import { GameModule } from './game/game.module';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 
 
 @Module({
   imports: [GameModule, UserModule, ProfileModule, GenreModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard //módulo que queremos proteger com o AuthGuard deve receber esse provider
+  }],
 })
 export class AppModule { }
