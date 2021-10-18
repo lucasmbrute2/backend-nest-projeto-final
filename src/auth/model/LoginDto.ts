e6810c07095f35a1b692ed205f460ff489f79df0
Lucas <lucasmbrute614@gmail.com>
2021-10-18 11:36:25 -0300

    chore(loginDto)=> changing length decorator

--- src/auth/model/LoginDto.ts
+++ src/auth/model/LoginDto.ts
@@ -1,12 +1,11 @@
-import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator"
+import { IsEmail, IsString, Length, Matches, } from "class-validator"
 
 export class LoginDto {
     @IsEmail()
     email: string
 
     @IsString()
-    @MinLength(4)
-    @MaxLength(20)
+    @Length(4, 20)
     @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
         message: 'password too weak',
     })
