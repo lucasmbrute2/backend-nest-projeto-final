ba2dfc14795fe43e469dd1ac6601a7914ce01fbb
Lucas <lucasmbrute614@gmail.com>
2021-10-18 11:38:31 -0300

    feat(auth.controller)=> implementing POST method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './model/LoginDto';
 
 @Controller()
 export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
     @Post('login')
     @HttpCode(HttpStatus.OK)
-    login() { }
+    login(@Body() dto: LoginDto) {
+        return this.authService.login(dto)
+    }
 }
